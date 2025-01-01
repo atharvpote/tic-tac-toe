8f4dbd7273286a60178a7d289cb1f715476628f4
Atharv Pote <atharvpote@gmail.com>
2025-01-01 10:50:13 +0530

    refactor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { Board } from "./_board";
 
 export default function Home() {
   return (
-    <main className="grid min-h-[100svh] items-center">
+    <main className="grid min-h-svh items-center">
       <div className="py-12">
         <header className="pb-12">
           <h1 className="text-center text-4xl font-bold sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl">
